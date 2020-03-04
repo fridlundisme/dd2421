0d1c1c6f004112478d13895560ad8da697851333
fridlund <friddelund@gmail.com>
2020-03-04 11:37:04 +0100

    Re-init lab3 as jupyter python script

--- lab3/lab3_jupyter.py
+++ lab3/lab3_jupyter.py
@@ -1,33 +1,31 @@
-#!/usr/bin/python
-# coding: utf-8
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
 
+# %% [markdown]
 # # Lab 3: Bayes Classifier and Boosting
-
-# ## Jupyter notebooks
-# 
-# In this lab, you can use Jupyter <https://jupyter.org/> to get a nice layout of your code and plots in one document. However, you may also use Python as usual, without Jupyter.
-# 
-# If you have Python and pip, you can install Jupyter with `sudo pip install jupyter`. Otherwise you can follow the instruction on <http://jupyter.readthedocs.org/en/latest/install.html>.
-# 
-# And that is everything you need! Now use a terminal to go into the folder with the provided lab files. Then run `jupyter notebook` to start a session in that folder. Click `lab3.ipynb` in the browser window that appeared to start this very notebook. You should click on the cells in order and either press `ctrl+enter` or `run cell` in the toolbar above to evaluate all the expressions.
-
+# %% [markdown]
+# Be sure to put `%matplotlib inline` at the top of every code cell where you call plotting functions to get the resulting plots inside the document.
+# %% [markdown]
 # ## Import the libraries
 # 
 # In Jupyter, select the cell below and press `ctrl + enter` to import the needed libraries.
 # Check out `labfuns.py` if you are interested in the details.
 
+# %%
 import numpy as np
 from scipy import misc
 from imp import reload
 from labfuns import *
 import random
 
-
+# %% [markdown]
 # ## Bayes classifier functions to implement
 # 
 # The lab descriptions state what each function should do.
 
-
+# %%
 # NOTE: you do not need to handle the W argument for this part!
 # in: labels - N vector of class labels
 # out: prior - C x 1 vector of class priors
@@ -42,15 +40,10 @@ def computePrior(labels, W=None):
 
     prior = np.zeros((Nclasses,1))
 
-    for c in classes:
-        prior = len(c)/Npts
-
     return prior
 
-
-
 # NOTE: you do not need to handle the W argument for this part!
-# in:      X - N x d matrix of N data points
+# in:      X - N x d matrix of N data points (set of row vectors)
 #     labels - N vector of class labels
 # out:    mu - C x d matrix of class means (mu[i] - class i mean)
 #      sigma - C x d x d matrix of class covariances (sigma[i] - class i sigma)
@@ -66,20 +59,6 @@ def mlParams(X, labels, W=None):
     mu = np.zeros((Nclasses,Ndims))
     my = np.zeros((Nclasses,Ndims))
     sigma = np.zeros((Nclasses,Ndims,Ndims))
-
-    # TODO: fill in the code to compute mu and sigma!
-    for jdx,c in enumerate(classes):
-        idx = labels==c # Extract the indicies for which y==class is TRUE
-        idx = np.where(labels==c)[0]
-        xlc = X[idx,:] # Get the x class for the class labels. Vectors are rows.
-        mu[jdx] = np.sum(xlc,axis=0)/len(idx)
-
-    for jdx,c in enumerate(classes):
-        idx = np.where(labels==c)[0]
-        xlc = X[idx,:]
-        diff = np.square(xlc - mu[jdx])
-        mean = np.sum(diff,axis=0) / len(idx)
-        sigma[jdx] = np.diag(mean)
 
     return mu, sigma
 
@@ -105,9 +84,7 @@ def classifyBayes(X, prior, mu, sigma):
     return h
 
 
-# The implemented functions can now be summarized into the `BayesClassifier` class, which we will use later to test the classifier, no need to add anything else here:
-
-
+# %%
 # NOTE: no need to touch this
 class BayesClassifier(object):
     def __init__(self):
@@ -123,36 +100,39 @@ class BayesClassifier(object):
     def classify(self, X):
         return classifyBayes(X, self.prior, self.mu, self.sigma)
 
-
+# %% [markdown]
 # ## Test the Maximum Likelihood estimates
 # 
 # Call `genBlobs` and `plotGaussian` to verify your estimates.
 
+# %%
+get_ipython().run_line_magic('matplotlib', 'inline')
 
 X, labels = genBlobs(centers=5)
 mu, sigma = mlParams(X,labels)
 plotGaussian(X,labels,mu,sigma)
 
-
+# %% [markdown]
 # Call the `testClassifier` and `plotBoundary` functions for this part.
 
-
-#testClassifier(BayesClassifier(), dataset='iris', split=0.7)
-
-
-
-#testClassifier(BayesClassifier(), dataset='vowel', split=0.7)
+# %%
+testClassifier(BayesClassifier(), dataset='iris', split=0.7)
 
 
+# %%
+testClassifier(BayesClassifier(), dataset='vowel', split=0.7)
 
-#plotBoundary(BayesClassifier(), dataset='iris',split=0.7)
 
+# %%
+get_ipython().run_line_magic('matplotlib', 'inline')
+plotBoundary(BayesClassifier(), dataset='iris',split=0.7)
 
+# %% [markdown]
 # ## Boosting functions to implement
 # 
 # The lab descriptions state what each function should do.
 
-
+# %%
 # in: base_classifier - a classifier of the type that we will boost, e.g. BayesClassifier
 #                   X - N x d matrix of N data points
 #              labels - N vector of class labels
@@ -208,10 +188,10 @@ def classifyBoost(X, classifiers, alphas, Nclasses):
         # one way to compute yPred after accumulating the votes
         return np.argmax(votes,axis=1)
 
-
+# %% [markdown]
 # The implemented functions can now be summarized another classifer, the `BoostClassifier` class. This class enables boosting different types of classifiers by initializing it with the `base_classifier` argument. No need to add anything here.
 
-
+# %%
 # NOTE: no need to touch this
 class BoostClassifier(object):
     def __init__(self, base_classifier, T=10):
@@ -229,77 +209,81 @@ class BoostClassifier(object):
     def classify(self, X):
         return classifyBoost(X, self.classifiers, self.alphas, self.nbr_classes)
 
-
+# %% [markdown]
 # ## Run some experiments
 # 
 # Call the `testClassifier` and `plotBoundary` functions for this part.
 
-
-#testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='iris',split=0.7)
-
-
-
-#testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='vowel',split=0.7)
+# %%
+testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='iris',split=0.7)
 
 
+# %%
+testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='vowel',split=0.7)
 
-#plotBoundary(BoostClassifier(BayesClassifier()), dataset='iris',split=0.7)
 
+# %%
+get_ipython().run_line_magic('matplotlib', 'inline')
+plotBoundary(BoostClassifier(BayesClassifier()), dataset='iris',split=0.7)
 
+# %% [markdown]
 # Now repeat the steps with a decision tree classifier.
 
-
-#testClassifier(DecisionTreeClassifier(), dataset='iris', split=0.7)
-
-
-
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
+# %%
+testClassifier(DecisionTreeClassifier(), dataset='iris', split=0.7)
 
 
-
-#testClassifier(DecisionTreeClassifier(), dataset='vowel',split=0.7)
-
-
-
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='vowel',split=0.7)
+# %%
+testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
 
 
-
-#plotBoundary(DecisionTreeClassifier(), dataset='iris',split=0.7)
-
-
-
-#plotBoundary(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
+# %%
+testClassifier(DecisionTreeClassifier(), dataset='vowel',split=0.7)
 
 
+# %%
+testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='vowel',split=0.7)
+
+
+# %%
+get_ipython().run_line_magic('matplotlib', 'inline')
+plotBoundary(DecisionTreeClassifier(), dataset='iris',split=0.7)
+
+
+# %%
+get_ipython().run_line_magic('matplotlib', 'inline')
+plotBoundary(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
+
+# %% [markdown]
 # ## Bonus: Visualize faces classified using boosted decision trees
 # 
 # Note that this part of the assignment is completely voluntary! First, let's check how a boosted decision tree classifier performs on the olivetti data. Note that we need to reduce the dimension a bit using PCA, as the original dimension of the image vectors is `64 x 64 = 4096` elements.
 
-
-#testClassifier(BayesClassifier(), dataset='olivetti',split=0.7, dim=20)
-
-
-
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='olivetti',split=0.7, dim=20)
+# %%
+testClassifier(BayesClassifier(), dataset='olivetti',split=0.7, dim=20)
 
 
+# %%
+testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='olivetti',split=0.7, dim=20)
+
+# %% [markdown]
 # You should get an accuracy around 70%. If you wish, you can compare this with using pure decision trees or a boosted bayes classifier. Not too bad, now let's try and classify a face as belonging to one of 40 persons!
 
-
-#X,y,pcadim = fetchDataset('olivetti') # fetch the olivetti data
-#xTr,yTr,xTe,yTe,trIdx,teIdx = trteSplitEven(X,y,0.7) # split into training and testing
-#pca = decomposition.PCA(n_components=20) # use PCA to reduce the dimension to 20
-#pca.fit(xTr) # use training data to fit the transform
-#xTrpca = pca.transform(xTr) # apply on training data
-#xTepca = pca.transform(xTe) # apply on test data
+# %%
+get_ipython().run_line_magic('matplotlib', 'inline')
+X,y,pcadim = fetchDataset('olivetti') # fetch the olivetti data
+xTr,yTr,xTe,yTe,trIdx,teIdx = trteSplitEven(X,y,0.7) # split into training and testing
+pca = decomposition.PCA(n_components=20) # use PCA to reduce the dimension to 20
+pca.fit(xTr) # use training data to fit the transform
+xTrpca = pca.transform(xTr) # apply on training data
+xTepca = pca.transform(xTe) # apply on test data
 # use our pre-defined decision tree classifier together with the implemented
 # boosting to classify data points in the training data
-#classifier = BoostClassifier(DecisionTreeClassifier(), T=10).trainClassifier(xTrpca, yTr)
-#yPr = classifier.classify(xTepca)
+classifier = BoostClassifier(DecisionTreeClassifier(), T=10).trainClassifier(xTrpca, yTr)
+yPr = classifier.classify(xTepca)
 # choose a test point to visualize
-#testind = random.randint(0, xTe.shape[0]-1)
+testind = random.randint(0, xTe.shape[0]-1)
 # visualize the test point together with the training points used to train
 # the class that the test point was classified to belong to
-#visualizeOlivettiVectors(xTr[yTr == yPr[testind],:], xTe[testind,:])
+visualizeOlivettiVectors(xTr[yTr == yPr[testind],:], xTe[testind,:])
 
